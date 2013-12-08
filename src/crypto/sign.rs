36132dc60696667687d55e0ba2384769b9a96075
dnaq <dnaq@users.noreply.github.com>
2013-12-08 18:22:16 +0100

    More documentation for crypto::sign

--- src/crypto/sign.rs
+++ src/crypto/sign.rs
@@ -11,6 +11,19 @@ chosen-message attacks.
 [Ed25519](http://ed25519.cr.yp.to/). This function is conjectured to meet the
 standard notion of unforgeability for a public-key signature scheme under
 chosen-message attacks.
+
+# Alternate primitives
+
+--------------------------------------------------------------------------------
+|crypto_sign                         | PUBLICKEYBYTES | SECRETKEYBYTES | BYTES |
+|------------------------------------|----------------|----------------|-------|
+|crypto_sign_ed25519                 | 32             | 64             | 64    |
+|crypto_sign_edwards25519sha512batch | 32             | 64             | 64    |
+--------------------------------------------------------------------------------
+
+crypto_sign_edwards25519sha512batch is a prototype. It has been replaced with
+Ed25519 and is only kept here for compatibility reasons.
+
 */
 pub use self::ed25519::*;
 pub mod ed25519;
