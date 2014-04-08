97172b5df375b1fce64fd8f9b321da67da8c2123
dnaq <dnaq@users.noreply.github.com>
2014-04-08 10:46:08 +0200

    Cleanup of imports

--- src/sodiumoxide/crypto/ed25519.rs
+++ src/sodiumoxide/crypto/ed25519.rs
@@ -6,7 +6,7 @@ chosen-message attacks.
 */
 extern crate serialize;
 use std::libc::{c_ulonglong, c_int};
-use std::slice::{from_elem};
+use std::slice::from_elem;
 
 #[link(name = "sodium")]
 extern {
