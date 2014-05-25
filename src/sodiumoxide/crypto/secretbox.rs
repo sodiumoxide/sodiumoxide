db75da028bc52d498d70662c8172316bb5672297
Michael Gehring <mg@ebfe.org>
2014-05-25 18:03:30 +0200

    work around new module/fs rules

--- src/sodiumoxide/crypto/secretbox.rs
+++ src/sodiumoxide/crypto/secretbox.rs
@@ -24,4 +24,5 @@ This function is conjectured to meet the standard notions of privacy and
 authenticity.
 */
 pub use self::xsalsa20poly1305::*;
+#[path="xsalsa20poly1305.rs"]
 pub mod xsalsa20poly1305;
