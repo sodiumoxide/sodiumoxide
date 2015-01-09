dd7da9b5193757c2b95bd2d380d849dd8d61cbf8
dnaq <dnaq@users.noreply.github.com>
2015-01-09 23:43:55 +0100

    Added #[allow(unstable)] because of libc, test-suite and slices
    We need to use libc for the external bindings, and libc isn't stable as
    of yet.
    
    Also for now slicing syntax doesn't seem to be working for most of the code
    that takes slices.
    
    The test-suite uses the test-crate which also isn't stable at the
    moment, it also uses the serialize-crate for some of the test vectors.
    We are supposed to use rustc-serialize instead, but for now I'm
    reluctant to add a test-only dependency to sodiumoxide, maybe if I
    figure out how to only make the test-suite depend on the rustc-serialize
    crate.

--- src/lib.rs
+++ src/lib.rs
@@ -51,9 +51,9 @@ generation functions and the key-generation functions are thread-safe.
 #![warn(non_upper_case_globals)]
 #![warn(non_camel_case_types)]
 #![warn(unused_qualifications)]
+#![allow(unstable)]
 
 extern crate "libsodium-sys" as ffi;
-#[allow(unstable)]
 extern crate libc;
 
 /**
