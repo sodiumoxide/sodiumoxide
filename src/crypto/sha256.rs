77f8980361e811b1fb7fcb0bfa050dc4c808487f
Daniel Ashhami <ashhami@gmail.com>
2015-01-31 14:25:45 +0100

    Added dev dependency on rustc-serialize
    The old serialize crate has been deprecated in favour of rustc-serialize

--- src/crypto/sha256.rs
+++ src/crypto/sha256.rs
@@ -8,7 +8,7 @@ inspire satisfactory levels of confidence. One can hope that NIST's
 SHA-3 competition will improve the situation.
 */
 #[cfg(test)]
-extern crate serialize;
+extern crate "rustc-serialize" as rustc_serialize;
 use ffi::{crypto_hash_sha256, crypto_hash_sha256_BYTES};
 use libc::c_ulonglong;
 
@@ -47,7 +47,7 @@ fn test_vector_2() {
 
 #[cfg(test)]
 fn test_nist_vector(filename: &str) {
-    use self::serialize::hex::{FromHex};
+    use self::rustc_serialize::hex::{FromHex};
     use std::path::Path;
     use std::old_io::BufferedReader;
     use std::old_io::File;
