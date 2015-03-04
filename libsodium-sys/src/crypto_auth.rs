c383b509af2d6accf066f38c1ae8d244264da667
dnaq <ashhami@gmail.com>
2015-03-04 11:52:19 +0100

    Updated libsodium-sys to build without warnings on latest rust-nightly

--- libsodium-sys/src/crypto_auth.rs
+++ libsodium-sys/src/crypto_auth.rs
@@ -33,7 +33,7 @@ fn test_crypto_auth_keybytes() {
 fn test_crypto_auth_primitive() {
     unsafe {
         let s = crypto_auth_primitive();
-        let s = std::ffi::c_str_to_bytes(&s);
+        let s = std::ffi::CStr::from_ptr(s).to_bytes();
         assert!(s == crypto_auth_PRIMITIVE.as_bytes());
     }
 }
