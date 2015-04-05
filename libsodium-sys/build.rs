19cc2fffcb42825e4c870323bd8edd0b02276cf8
Markus Kobler <markus@distinctive.co>
2015-04-05 11:28:36 +0100

    Enable static linking support (similar to openssl-sys)

--- libsodium-sys/build.rs
+++ libsodium-sys/build.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main () {
-    println!("cargo:rustc-flags=-l sodium");
+    if let Some(lib_dir) = env::var("SODIUM_LIB_DIR").ok() {
+    	println!("cargo:rustc-flags=-L native={}", lib_dir);
+    }
+
+    let mode = if env::var_os("SODIUM_STATIC").is_some() {
+    	"static"
+    } else {
+    	"dylib"
+    };
+
+    println!("cargo:rustc-flags=-l {0}=sodium", mode);
 }
