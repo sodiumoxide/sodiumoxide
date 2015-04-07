0b996d0af5bbdf39f8231344bd372eddf9ecbfc4
dnaq <ashhami@gmail.com>
2015-04-07 12:15:07 +0200

    Cleanup of patch from @markuskobler

--- libsodium-sys/build.rs
+++ libsodium-sys/build.rs
@@ -1,14 +1,13 @@
 use std::env;
 
 fn main () {
-    if let Some(lib_dir) = env::var("SODIUM_LIB_DIR").ok() {
+    if let Ok(lib_dir) = env::var("SODIUM_LIB_DIR") {
     	println!("cargo:rustc-flags=-L native={}", lib_dir);
     }
 
-    let mode = if env::var_os("SODIUM_STATIC").is_some() {
-    	"static"
-    } else {
-    	"dylib"
+    let mode = match env::var_os("SODIUM_STATIC") {
+        Some(_) => "static",
+        None => "dylib"
     };
 
     println!("cargo:rustc-flags=-l {0}=sodium", mode);
