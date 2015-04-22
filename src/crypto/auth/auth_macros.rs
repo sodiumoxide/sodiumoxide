97ba41ce60e1d2f9e0da64ea96ba74fb5b923963
dnaq <dnaq@users.noreply.github.com>
2015-04-22 13:56:09 +0200

    Remove extraneous whitespace

--- src/crypto/auth/auth_macros.rs
+++ src/crypto/auth/auth_macros.rs
@@ -1,7 +1,7 @@
-macro_rules! auth_module (($auth_name:ident, 
-                           $verify_name:ident, 
-                           $verify_fn:ident, 
-                           $keybytes:expr, 
+macro_rules! auth_module (($auth_name:ident,
+                           $verify_name:ident,
+                           $verify_fn:ident,
+                           $keybytes:expr,
                            $tagbytes:expr) => (
 
 use libc::c_ulonglong;
