8b5e520589661a237b27ac57e3d7c8b49bf3fd7d
AurelR <aurel.rothaermel@gmail.com>
2015-05-04 14:12:50 +0200

    Add hmacsha512

--- src/crypto/auth/mod.rs
+++ src/crypto/auth/mod.rs
@@ -30,6 +30,7 @@
 //! |-------------------------|-----------------|-----|--------|
 //! |crypto_auth_hmacsha256   |HMAC_SHA-256     |32   |32      |
 //! |crypto_auth_hmacsha512256|HMAC_SHA-512-256 |32   |32      |
+//! |crypto_auth_hmacsha512   |HMAC_SHA-512     |64   |32      |
 //! ------------------------------------------------------------
 //! # Example
 //! ```
@@ -43,5 +44,6 @@
 pub use self::hmacsha512256::*;
 #[macro_use]
 mod auth_macros;
+pub mod hmacsha512;
 pub mod hmacsha512256;
 pub mod hmacsha256;
