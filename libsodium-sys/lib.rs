f27ab62c30222722ae55addf82aafa33e2f5a180
AurelR <aurel.rothaermel@gmail.com>
2015-05-10 21:32:39 +0200

    Add bindings to crypto_core_salsa20.h

--- libsodium-sys/lib.rs
+++ libsodium-sys/lib.rs
@@ -16,6 +16,7 @@ include!("src/crypto_box.rs");
 include!("src/crypto_box_curve25519xsalsa20poly1305.rs");
 
 include!("src/crypto_core_hsalsa20.rs");
+include!("src/crypto_core_salsa20.rs");
 
 include!("src/crypto_hash.rs");
 include!("src/crypto_hash_sha256.rs");
