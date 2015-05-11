1dfb3625402333ea7b1de8e31a9b2a52bae51939
dnaq <ashhami@gmail.com>
2015-05-11 10:19:15 +0200

    whitespace changes

--- libsodium-sys/src/crypto_core_salsa20.rs
+++ libsodium-sys/src/crypto_core_salsa20.rs
@@ -5,7 +5,6 @@ pub const crypto_core_salsa20_INPUTBYTES: usize = 16;
 pub const crypto_core_salsa20_KEYBYTES: usize = 32;
 pub const crypto_core_salsa20_CONSTBYTES: usize = 16;
 
-
 extern {
     pub fn crypto_core_salsa20_outputbytes() -> size_t;
     pub fn crypto_core_salsa20_inputbytes() -> size_t;
@@ -19,25 +18,27 @@ extern {
         c: *const [u8; crypto_core_salsa20_CONSTBYTES]) -> c_int;
 }
 
-
 #[test]
 fn test_crypto_core_salsa20_outputbytes() {
     assert!(unsafe {
         crypto_core_salsa20_outputbytes() as usize
     } == crypto_core_salsa20_OUTPUTBYTES)
 }
+
 #[test]
 fn test_crypto_core_salsa20_inputbytes() {
     assert!(unsafe {
         crypto_core_salsa20_inputbytes() as usize
     } == crypto_core_salsa20_INPUTBYTES)
 }
+
 #[test]
 fn test_crypto_core_salsa20_keybytes() {
     assert!(unsafe {
         crypto_core_salsa20_keybytes() as usize
     } == crypto_core_salsa20_KEYBYTES)
 }
+
 #[test]
 fn test_crypto_core_salsa20_constbytes() {
     assert!(unsafe {
