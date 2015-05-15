8ce48f7f5f2d71205daf0585ea0c7035eff5b521
Daniel Ashhami <ashhami@gmail.com>
2015-05-15 10:49:08 +0200

    Use pkg-config when building libsodium-sys
    Closes #65. This commit also removes support for the SODIUM_STATIC and
    SODIUM_LIB_DIR environment variables for now.

--- libsodium-sys/build.rs
+++ libsodium-sys/build.rs
@@ -1,14 +1,5 @@
-use std::env;
+extern crate pkg_config;
 
-fn main () {
-    if let Ok(lib_dir) = env::var("SODIUM_LIB_DIR") {
-    	println!("cargo:rustc-flags=-L native={}", lib_dir);
-    }
-
-    let mode = match env::var_os("SODIUM_STATIC") {
-        Some(_) => "static",
-        None => "dylib"
-    };
-
-    println!("cargo:rustc-flags=-l {0}=sodium", mode);
+fn main() {
+    pkg_config::find_library("libsodium").unwrap();
 }
