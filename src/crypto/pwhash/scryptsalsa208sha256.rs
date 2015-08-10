f49b036459a3c7a360c68ee8fcdcc477a13123a8
dnaq <dnaq@users.noreply.github.com>
2015-08-10 10:22:14 +0200

    Removed `non_secret_newtype_impl` from `HashedPassword`
    A `HashedPassword` should still be considered secret data, and
    allowing the standard `Ord` and `PartialOrd` traits on it might lead
    to side-channel attacks if a user does manual comparison of `HashedPassword`s

--- src/crypto/pwhash/scryptsalsa208sha256.rs
+++ src/crypto/pwhash/scryptsalsa208sha256.rs
@@ -48,7 +48,6 @@ newtype_impl!(Salt, SALTBYTES);
 pub struct HashedPassword(pub [u8; STRBYTES]);
 newtype_clone!(HashedPassword);
 newtype_impl!(HashedPassword, STRBYTES);
-non_secret_newtype_impl!(HashedPassword);
 
 /// `gen_salt()` randombly generates a new `Salt` for key derivation
 ///
