127b3763a548830ea112b17fc78b375d500d3d91
Paul Barker <paul@paulbarker.me.uk>
2016-01-16 19:05:44 +0000

    Add further tests for crypto_generichash
    Add tests for the bindings of:
    * crypto_generichash_statebytes
    * crypto_generichash
    * crypto_generichash_init
    * crypto_generichash_update
    * crypto_generichash_final
    
    These tests only check that the bindings can be called and report success by
    returning zero where appropriate. The hash algorithm itself is not tested here.

--- libsodium-sys/src/crypto_generichash.rs
+++ libsodium-sys/src/crypto_generichash.rs
@@ -94,3 +94,55 @@ fn test_crypto_generichash_primitive() {
         assert_eq!(s, crypto_generichash_PRIMITIVE.as_bytes());
     }
 }
+
+#[test]
+fn test_crypto_generichash_statebytes() {
+    assert!(unsafe { crypto_generichash_statebytes() } > 0);
+}
+
+#[test]
+fn test_crypto_generichash() {
+    let mut out = [0u8; crypto_generichash_BYTES];
+    let m = [0u8; 64];
+    let key = [0u8; crypto_generichash_KEYBYTES];
+
+    assert_eq!(unsafe {
+        crypto_generichash(out.as_mut_ptr(), out.len(),
+                           m.as_ptr(), m.len() as u64,
+                           key.as_ptr(), key.len())
+    }, 0);
+}
+
+#[cfg(test)]
+use std::mem;
+
+#[test]
+fn test_crypto_generichash_multipart() {
+    let mut out = [0u8; crypto_generichash_BYTES];
+    let m = [0u8; 64];
+    let key = [0u8; crypto_generichash_KEYBYTES];
+
+    let mut st = vec![0u8; (unsafe { crypto_generichash_statebytes() })];
+    let pst = unsafe { mem::transmute::<*mut u8, *mut crypto_generichash_state>(st.as_mut_ptr()) };
+
+    assert_eq!(unsafe {
+        crypto_generichash_init(pst,
+                                key.as_ptr(), key.len(),
+                                out.len())
+    }, 0);
+
+    assert_eq!(unsafe {
+        crypto_generichash_update(pst,
+                                m.as_ptr(), m.len() as u64)
+    }, 0);
+
+    assert_eq!(unsafe {
+        crypto_generichash_update(pst,
+                                m.as_ptr(), m.len() as u64)
+    }, 0);
+
+    assert_eq!(unsafe {
+        crypto_generichash_final(pst,
+                                out.as_mut_ptr(), out.len())
+    }, 0);
+}
