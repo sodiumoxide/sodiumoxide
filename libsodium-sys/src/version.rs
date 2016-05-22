7982b812dc3bb2ef3380128be40c03321a6398a2
Dan Larkin <dan@danlarkin.org>
2016-05-22 11:13:50 -0700

    test if s_l_v_{major, minor} can be cast to usize

--- libsodium-sys/src/version.rs
+++ libsodium-sys/src/version.rs
@@ -5,3 +5,13 @@ extern {
     pub fn sodium_library_version_major() -> c_int;
     pub fn sodium_library_version_minor() -> c_int;
 }
+
+#[test]
+fn test_sodium_library_version_major() {
+    assert!(unsafe { sodium_library_version_major() } > 0)
+}
+
+#[test]
+fn test_sodium_library_version_minor() {
+    assert!(unsafe { sodium_library_version_minor() } >= 0)
+}
