0a81800e6642dc4b422db28d096ac2409e6777c8
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-08-02 09:06:56 +0100

    Updated libsodium dependency to 1.0.11 for the 'get-libsodium' feature.

--- libsodium-sys/build.rs
+++ libsodium-sys/build.rs
@@ -30,11 +30,13 @@ fn main() {
 
 #[cfg(feature = "get-libsodium")]
 extern crate gcc;
-
-#[cfg(all(windows, feature = "get-libsodium"))]
+#[cfg(feature = "get-libsodium")]
 extern crate flate2;
-#[cfg(all(windows, feature = "get-libsodium"))]
+#[cfg(feature = "get-libsodium")]
 extern crate tar;
+
+#[cfg(feature = "get-libsodium")]
+const VERSION: &'static str = "1.0.11";
 
 #[cfg(feature = "get-libsodium")]
 fn get_install_dir() -> String {
@@ -45,12 +47,10 @@ fn get_install_dir() -> String {
 #[cfg(all(windows, feature = "get-libsodium"))]
 fn main() {
     use std::fs::{self, File};
-    use std::path::Path;
+    use std::path::{Path, PathBuf};
     use std::process::Command;
     use flate2::read::GzDecoder;
     use tar::Archive;
-
-    const VERSION: &'static str = "1.0.10";
 
     if cfg!(target_env = "msvc") {
         panic!("This feature currently can't be used with MSVC builds.");
@@ -61,23 +61,22 @@ fn main() {
     let gz_filename = basename.clone() + "-mingw.tar.gz";
     let url = "https://download.libsodium.org/libsodium/releases/".to_string() + &gz_filename;
     let install_dir = get_install_dir();
-    let gz_path = install_dir.clone() + &gz_filename;
+    let gz_path = install_dir.clone() + "/" + &gz_filename;
     unwrap!(fs::create_dir_all(&install_dir));
 
-    let wget_output = Command::new("powershell")
+    let command = "(New-Object System.Net.WebClient).DownloadFile(\"".to_string() + &url +
+                  "\", \"" + &gz_path + "\")";
+    let download_output = Command::new("powershell")
         .arg("-Command")
-        .arg("wget")
-        .arg(&url)
-        .arg("-OutFile")
-        .arg(&gz_path)
+        .arg(&command)
         .output()
         .unwrap_or_else(|error| {
-            panic!("Failed to run powershell wget command: {}", error);
+            panic!("Failed to run powershell download command: {}", error);
         });
-    if !wget_output.status.success() {
+    if !download_output.status.success() {
         panic!("\n{}\n{}\n",
-               String::from_utf8_lossy(&wget_output.stdout),
-               String::from_utf8_lossy(&wget_output.stderr));
+               String::from_utf8_lossy(&download_output.stdout),
+               String::from_utf8_lossy(&download_output.stderr));
     }
 
     // Unpack the tarball
@@ -102,8 +101,31 @@ fn main() {
     // Clean up
     let _ = fs::remove_file(gz_path);
 
+    // Get path to gcc in order to guess location of libpthread.a
+    let where_output = Command::new("where")
+        .arg(gcc::Config::new().get_compiler().path())
+        .output()
+        .unwrap_or_else(|error| {
+            panic!("Failed to run where command: {}", error);
+        });
+    if !where_output.status.success() {
+        panic!("\n{}\n{}\n",
+               String::from_utf8_lossy(&where_output.stdout),
+               String::from_utf8_lossy(&where_output.stderr));
+    }
+    let compiler_path_as_string = String::from_utf8_lossy(&where_output.stdout);
+    let compiler_path = PathBuf::from(compiler_path_as_string.trim());
+    let mingw_path = unwrap!(unwrap!(compiler_path.parent()).parent());
+    let lib_path = if cfg!(target_pointer_width = "32") {
+        mingw_path.join("lib")
+    } else {
+        mingw_path.join("x86_64-w64-mingw32").join("lib")
+    };
+
     println!("cargo:rustc-link-lib=static=sodium");
+    println!("cargo:rustc-link-lib=pthread");
     println!("cargo:rustc-link-search=native={}", install_dir);
+    println!("cargo:rustc-link-search=native={}", lib_path.display());
 }
 
 
@@ -111,18 +133,51 @@ fn main() {
 #[cfg(all(not(windows), feature = "get-libsodium"))]
 fn main() {
     use std::env;
+    use std::fs::{self, File};
     use std::process::Command;
+    use flate2::read::GzDecoder;
+    use tar::Archive;
 
-    let _ = Command::new("git").args(&["submodule", "update", "--init"]).status();
+    // Download gz tarball
+    let basename = "libsodium-".to_string() + VERSION;
+    let gz_filename = basename.clone() + ".tar.gz";
+    let url = "https://download.libsodium.org/libsodium/releases/".to_string() + &gz_filename;
+    let install_dir = get_install_dir();
+    let gz_path = install_dir.clone() + "/" + &gz_filename;
+    unwrap!(fs::create_dir_all(&install_dir));
+
+    let curl_output = Command::new("curl")
+        .arg(&url)
+        .arg("-o")
+        .arg(&gz_path)
+        .output()
+        .unwrap_or_else(|error| {
+            panic!("Failed to run curl command: {}", error);
+        });
+    if !curl_output.status.success() {
+        panic!("\n{}\n{}\n",
+               String::from_utf8_lossy(&curl_output.stdout),
+               String::from_utf8_lossy(&curl_output.stderr));
+    }
+
+    // Unpack the tarball
+    let gz_archive = unwrap!(File::open(&gz_path));
+    let gz_decoder = unwrap!(GzDecoder::new(gz_archive));
+    let mut archive = Archive::new(gz_decoder);
+    unwrap!(archive.unpack("."));
+
+    // Clean up
+    let _ = fs::remove_file(gz_path);
+
+    // Run `./configure`
     let gcc = gcc::Config::new();
     let cc = format!("{}", gcc.get_compiler().path().display());
-    let install_dir = get_install_dir();
     let prefix_arg = format!("--prefix={}", install_dir);
     let target = unwrap!(env::var("TARGET"));
     let host_arg = format!("--host={}", target);
 
     let configure_output = Command::new("./configure")
-        .current_dir("libsodium")
+        .current_dir(&basename)
         .env("CC", &cc)
         .arg(&prefix_arg)
         .arg(&host_arg)
@@ -137,9 +192,10 @@ fn main() {
                String::from_utf8_lossy(&configure_output.stderr));
     }
 
+    // Run `make check`
     let j_arg = format!("-j{}", unwrap!(env::var("NUM_JOBS")));
     let make_output = Command::new("make")
-        .current_dir("libsodium")
+        .current_dir(&basename)
         .env("V", "1")
         .arg("check")
         .arg(&j_arg)
@@ -154,8 +210,9 @@ fn main() {
                String::from_utf8_lossy(&make_output.stderr));
     }
 
+    // Run `make install`
     let install_output = Command::new("make")
-        .current_dir("libsodium")
+        .current_dir(&basename)
         .arg("install")
         .output()
         .unwrap_or_else(|error| {
