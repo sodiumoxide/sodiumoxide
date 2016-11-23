ccdab29084a526edaf3e2e3dd913969f49c38685
Danilo Bargen <mail@dbrgn.ch>
2016-11-23 22:51:20 +0100

    Add randombytes_uniform function

--- src/randombytes.rs
+++ src/randombytes.rs
@@ -26,3 +26,38 @@ pub fn randombytes_into(buf: &mut [u8]) {
         ffi::randombytes_buf(buf.as_mut_ptr(), buf.len());
     }
 }
+
+/// `randombytes_uniform()` returns an unpredictable value between 0 and
+/// `upper_bound` (excluded). It does its best to guarantee a uniform
+/// distribution of the possible output values.
+///
+/// THREAD SAFETY: `randombytes_uniform()` is thread-safe provided that you
+/// have called `sodiumoxide::init()` once before using any other function
+/// from sodiumoxide.
+pub fn randombytes_uniform(upper_bound: u32) -> u32 {
+    unsafe {
+        ffi::randombytes_uniform(upper_bound)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_randombytes_uniform_lt1000() {
+        for _ in 0..100 {
+            let random = randombytes_uniform(1000);
+            assert!(random < 1000);
+        }
+    }
+
+    #[test]
+    fn test_randombytes_uniform_lt3() {
+        for _ in 0..100 {
+            let random = randombytes_uniform(3);
+            assert!(random < 3);
+        }
+    }
+
+}
