78794a068c49a542237156b1022d6c3ca5fd170d
Daniel Ashhami <dnaq@users.noreply.github.com>
2017-02-13 13:57:47 +0100

    Add testcase for round-trip with msgpack #152

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,5 +1,6 @@
 #![cfg(all(test, feature = "default"))]
 extern crate serde_json;
+extern crate rmp_serde;
 extern crate core;
 use serde::{Serialize, Deserialize};
 
@@ -9,5 +10,11 @@ pub fn round_trip<T>(value: T)
 {
     let encoded_value = serde_json::to_string(&value).unwrap();
     let decoded_value = serde_json::from_str(&encoded_value).unwrap();
+    assert_eq!(value, decoded_value);
+
+    let mut buf = Vec::new();
+    value.serialize(&mut rmp_serde::Serializer::new(&mut buf)).unwrap();
+    let mut de = rmp_serde::Deserializer::new(&buf[..]);
+    let decoded_value = Deserialize::deserialize(&mut de).unwrap();
     assert_eq!(value, decoded_value);
 }
