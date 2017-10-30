a6efd1829f7090ae1af21c6286fb59aaebfe21db
dnaq <dnaq@users.noreply.github.com>
2017-10-30 21:37:30 +0100

    Updated streaming auth functions to have the same api as streaming hash functions

--- src/crypto/auth/auth_macros.rs
+++ src/crypto/auth/auth_macros.rs
@@ -218,18 +218,17 @@ impl State {
     /// `update()` can be called more than once in order to compute the authenticator
     /// from sequential chunks of the message.
     pub fn update(&mut self, in_: &[u8]) {
-        let &mut State(ref mut state) = self;
         unsafe {
-            $update_name(state, in_.as_ptr(), in_.len() as c_ulonglong);
+            $update_name(&mut self.0, in_.as_ptr(), in_.len() as c_ulonglong);
         }
     }
 
-    /// `finalize()` finalizes the authenticator computation and returns a `Tag`.
-    pub fn finalize(&mut self) -> Tag {
+    /// `finalize()` finalizes the authenticator computation and returns a `Tag`. `finalize`
+    /// consumes the `State` so that it cannot be accidentally reused.
+    pub fn finalize(mut self) -> Tag {
         unsafe {
-            let &mut State(ref mut state) = self;
             let mut tag = [0; $tagbytes as usize];
-            $final_name(state, &mut tag);
+            $final_name(&mut self.0, &mut tag);
             Tag(tag)
         }
     }
