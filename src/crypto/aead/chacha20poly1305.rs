ae5fa5f06ae0e2cdedc18d61ef7e954567f350c5
pe@pijul.org <pe@pijul.org>
2017-12-02 17:01:04 +0100

    More accurate documentation, and test vector for the Chacha20-Poly1305 AEAD construction

--- src/crypto/aead/chacha20poly1305.rs
+++ src/crypto/aead/chacha20poly1305.rs
@@ -1,37 +1,19 @@
-//! The original NaCl variant of the Chacha20-Poly1305 construction,
-//! an algorithm for Authenticated Encryption with Additional Data
+//! The original variant of the Chacha20-Poly1305 construction, an
+//! algorithm for Authenticated Encryption with Additional Data
 //! (AEAD).
 
 #[cfg(not(feature = "std"))] use prelude::*;
 use ffi;
 use libc::{c_ulonglong};
 use std;
-
-/// Number of bytes in `Key`.
-pub const KEYBYTES: usize = ffi::crypto_aead_chacha20poly1305_KEYBYTES;
+pub use crypto::stream::chacha20::{Key, Nonce, KEYBYTES, NONCEBYTES};
 
 /// Number of bytes in a `Tag`.
 pub const ABYTES: usize = ffi::crypto_aead_chacha20poly1305_ABYTES;
 
-/// Number of bytes in a `Nonce`.
-pub const NPUBBYTES: usize = ffi::crypto_aead_chacha20poly1305_NPUBBYTES;
-
-new_type! {
-    /// `Key` for symmetric authenticated encryption
-    ///
-    /// When a `Key` goes out of scope its contents
-    /// will be zeroed out
-    secret Key(KEYBYTES);
-}
-
-new_type! {
-    /// `Nonce` for symmetric authenticated encryption
-    nonce Nonce(NPUBBYTES);
-}
-
 new_type! {
     /// `Mac` for symmetric authenticated encryption
-    nonce Mac(ABYTES);
+    public Mac(ABYTES);
 }
 
 /// Verify that the ciphertext `ciphertext` (as produced by
@@ -73,6 +55,35 @@ pub fn decrypt(decrypted: &mut [u8], ciphertext: &[u8], ad: Option<&[u8]>, nonce
     }
 }
 
+/// Same as `decrypt`, but modifies the message in place instead of
+/// requiring a different slice for the output (in particular, this
+/// function does not panic).
+pub fn decrypt_in_place(ciphertext: &mut [u8], ad: Option<&[u8]>, nonce: &Nonce, key: &Key) -> Result<usize, ()> {
+    unsafe {
+        let mut mlen: c_ulonglong = ciphertext.len() as c_ulonglong;
+        let (ad_p, ad_len) = if let Some(ad) = ad {
+            (ad.as_ptr(), ad.len() as c_ulonglong)
+        } else {
+            (std::ptr::null(), 0)
+        };
+        if ffi::crypto_aead_chacha20poly1305_decrypt(
+            ciphertext.as_mut_ptr(),
+            &mut mlen,
+            std::ptr::null_mut(),
+            ciphertext.as_ptr(),
+            ciphertext.len() as c_ulonglong,
+            ad_p,
+            ad_len,
+            &nonce.0,
+            &key.0
+        ) == 0 {
+            Ok(mlen as usize)
+        } else {
+            Err(())
+        }
+    }
+}
+
 
 /// Encrypt a message m with a key `key` and a nonce `nonce`. It puts
 /// the resulting ciphertext, whose length is equal to the message,
@@ -82,7 +93,7 @@ pub fn decrypt(decrypted: &mut [u8], ciphertext: &[u8], ad: Option<&[u8]>, nonce
 ///
 /// This function panics if `ciphertext` is not of length at least
 /// `message.len() + ABYTES`.
-pub fn encrypt(ciphertext: &mut [u8], message: &[u8], ad: Option<&[u8]>, nonce: &Nonce, key: &Key) -> Result<usize, ()> {
+pub fn encrypt(ciphertext: &mut [u8], message: &[u8], ad: Option<&[u8]>, nonce: &Nonce, key: &Key) -> usize {
     unsafe {
         let mut clen: c_ulonglong = ciphertext.len() as c_ulonglong;
         assert!(ciphertext.len() >= message.len() + ABYTES);
@@ -91,7 +102,7 @@ pub fn encrypt(ciphertext: &mut [u8], message: &[u8], ad: Option<&[u8]>, nonce: 
         } else {
             (std::ptr::null(), 0)
         };
-        if ffi::crypto_aead_chacha20poly1305_encrypt(
+        ffi::crypto_aead_chacha20poly1305_encrypt(
             ciphertext.as_mut_ptr(),
             &mut clen,
             message.as_ptr(),
@@ -101,11 +112,34 @@ pub fn encrypt(ciphertext: &mut [u8], message: &[u8], ad: Option<&[u8]>, nonce: 
             std::ptr::null_mut(),
             &nonce.0,
             &key.0
-        ) == 0 {
-            Ok(clen as usize)
+        );
+        clen as usize
+    }
+}
+
+/// Same as `encrypt`, but modifies the message in place instead of
+/// requiring a different slice for the output (in particular, this
+/// function does not panic).
+pub fn encrypt_in_place(message: &mut [u8], ad: Option<&[u8]>, nonce: &Nonce, key: &Key) -> usize {
+    unsafe {
+        let mut clen: c_ulonglong = message.len() as c_ulonglong;
+        let (ad_p, ad_len) = if let Some(ad) = ad {
+            (ad.as_ptr(), ad.len() as c_ulonglong)
         } else {
-            Err(())
-        }
+            (std::ptr::null(), 0)
+        };
+        ffi::crypto_aead_chacha20poly1305_encrypt(
+            message.as_mut_ptr(),
+            &mut clen,
+            message.as_ptr(),
+            message.len() as c_ulonglong,
+            ad_p,
+            ad_len,
+            std::ptr::null_mut(),
+            &nonce.0,
+            &key.0
+        );
+        clen as usize
     }
 }
 
@@ -146,6 +180,33 @@ pub fn decrypt_detached(decrypted: &mut [u8], ciphertext: &[u8], mac: &Mac, ad: 
     }
 }
 
+/// Same as `decrypt_detached`, but modifies the message in place
+/// instead of requiring a different slice for the output.
+pub fn decrypt_detached_in_place(ciphertext: &mut [u8], mac: &Mac, ad: Option<&[u8]>, nonce: &Nonce, key: &Key) -> Result<(), ()> {
+    unsafe {
+        let (ad_p, ad_len) = if let Some(ad) = ad {
+            (ad.as_ptr(), ad.len() as c_ulonglong)
+        } else {
+            (std::ptr::null(), 0)
+        };
+        if ffi::crypto_aead_chacha20poly1305_decrypt_detached(
+            ciphertext.as_mut_ptr(),
+            std::ptr::null_mut(),
+            ciphertext.as_ptr(),
+            ciphertext.len() as c_ulonglong,
+            mac.0.as_ptr(),
+            ad_p,
+            ad_len,
+            &nonce.0,
+            &key.0
+        ) == 0 {
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+}
+
 
 /// Encrypt a message `message` with a key `key` and a nonce
 /// `nonce`. It puts the resulting ciphertext, whose length is equal
@@ -158,7 +219,7 @@ pub fn decrypt_detached(decrypted: &mut [u8], ciphertext: &[u8], mac: &Mac, ad: 
 ///
 /// This function panics if `message` and `ciphertext` are of
 /// different sizes.
-pub fn encrypt_detached(ciphertext: &mut [u8], mac: &mut Mac, message: &[u8], ad: Option<&[u8]>, nonce: &Nonce, key: &Key) -> Result<(), ()> {
+pub fn encrypt_detached(ciphertext: &mut [u8], mac: &mut Mac, message: &[u8], ad: Option<&[u8]>, nonce: &Nonce, key: &Key) {
     unsafe {
         assert!(ciphertext.len() == message.len());
         let (ad_p, ad_len) = if let Some(ad) = ad {
@@ -167,7 +228,7 @@ pub fn encrypt_detached(ciphertext: &mut [u8], mac: &mut Mac, message: &[u8], ad
             (std::ptr::null(), 0)
         };
         let mut maclen = ABYTES as c_ulonglong;
-        if ffi::crypto_aead_chacha20poly1305_encrypt_detached(
+        ffi::crypto_aead_chacha20poly1305_encrypt_detached(
             ciphertext.as_mut_ptr(),
             mac.0.as_mut_ptr(),
             &mut maclen,
@@ -178,10 +239,59 @@ pub fn encrypt_detached(ciphertext: &mut [u8], mac: &mut Mac, message: &[u8], ad
             std::ptr::null_mut(),
             &nonce.0,
             &key.0
-        ) == 0 {
-            Ok(())
+        );
+    }
+}
+
+/// Same as `encrypt_detached`, but modifies the message in place
+/// instead of requiring a different slice for the output.
+pub fn encrypt_detached_in_place(message: &mut [u8], mac: &mut Mac, ad: Option<&[u8]>, nonce: &Nonce, key: &Key) {
+    unsafe {
+        let (ad_p, ad_len) = if let Some(ad) = ad {
+            (ad.as_ptr(), ad.len() as c_ulonglong)
         } else {
-            Err(())
-        }
+            (std::ptr::null(), 0)
+        };
+        let mut maclen = ABYTES as c_ulonglong;
+        ffi::crypto_aead_chacha20poly1305_encrypt_detached(
+            message.as_mut_ptr(),
+            mac.0.as_mut_ptr(),
+            &mut maclen,
+            message.as_ptr(),
+            message.len() as c_ulonglong,
+            ad_p,
+            ad_len,
+            std::ptr::null_mut(),
+            &nonce.0,
+            &key.0
+        );
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    // Test from https://tools.ietf.org/html/draft-agl-tls-chacha20poly1305-04#section-7
+    #[test]
+    fn test_seal_open() {
+        let plaintext = &[0x86,0xd0,0x99,0x74,0x84,0x0b,0xde,0xd2,0xa5,0xca];
+        let key = Key([0x42,0x90,0xbc,0xb1,0x54,0x17,0x35,0x31,
+                       0xf3,0x14,0xaf,0x57,0xf3,0xbe,0x3b,0x50,
+                       0x06,0xda,0x37,0x1e,0xce,0x27,0x2a,0xfa,
+                       0x1b,0x5d,0xbd,0xd1,0x10,0x0a,0x10,0x07]);
+        let nonce = Nonce([0xcd,0x7c,0xf6,0x7b,0xe3,0x9c,0x79,0x4a]);
+        let aad = &[0x87,0xe2,0x29,0xd4,0x50,0x08,0x45,0xa0,0x79,0xc0];
+
+        let output = &[0xe3,0xe4,0x46,0xf7,0xed,0xe9,0xa1,0x9b,
+                       0x62,0xa4,0x67,0x7d,0xab,0xf4,0xe3,0xd2,
+                       0x4b,0x87,0x6b,0xb2,0x84,0x75,0x38,0x96,
+                       0xe1,0xd6];
+
+
+        let mut ciphertext = vec![0; output.len()];
+        let n = encrypt(&mut ciphertext, plaintext, Some(aad), &nonce, &key);
+        assert_eq!(n, output.len());
+        assert_eq!(&ciphertext[..], output);
     }
 }
