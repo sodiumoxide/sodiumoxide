e1fec6344d9c9c4401f449841760676588dfedea
Árpád Goretity <h2co3@h2co3.org>
2017-12-24 01:06:11 +0100

    Add safe bindings to sodium_pad() and sodium_unpad()

--- libsodium-sys/src/utils.rs
+++ libsodium-sys/src/utils.rs
@@ -15,4 +15,14 @@ extern {
     pub fn sodium_mprotect_noaccess(ptr: *const c_void) -> c_int;
     pub fn sodium_mprotect_readonly(ptr: *const c_void) -> c_int;
     pub fn sodium_mprotect_readwrite(ptr: *const c_void) -> c_int;
+
+    pub fn sodium_pad(padded_buflen_p: *mut usize,
+                  buf: *mut u8,
+                  unpadded_buflen: usize,
+                  blocksize: usize,
+                  max_buflen: usize) -> c_int;
+    pub fn sodium_unpad(unpadded_buflen_p: *mut usize,
+                    buf: *const u8,
+                    padded_buflen: usize,
+                    blocksize: usize) -> c_int;
 }
