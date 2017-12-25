80361215a94b71fc0a30d4852c56f93e4e4a0a27
Árpád Goretity <h2co3@h2co3.org>
2017-12-25 12:34:45 +0100

    fix indentation

--- libsodium-sys/src/utils.rs
+++ libsodium-sys/src/utils.rs
@@ -17,12 +17,12 @@ extern {
     pub fn sodium_mprotect_readwrite(ptr: *const c_void) -> c_int;
 
     pub fn sodium_pad(padded_buflen_p: *mut usize,
-                  buf: *mut u8,
-                  unpadded_buflen: usize,
-                  blocksize: usize,
-                  max_buflen: usize) -> c_int;
+                      buf: *mut u8,
+                      unpadded_buflen: usize,
+                      blocksize: usize,
+                      max_buflen: usize) -> c_int;
     pub fn sodium_unpad(unpadded_buflen_p: *mut usize,
-                    buf: *const u8,
-                    padded_buflen: usize,
-                    blocksize: usize) -> c_int;
+                        buf: *const u8,
+                        padded_buflen: usize,
+                        blocksize: usize) -> c_int;
 }
