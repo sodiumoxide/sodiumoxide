48532246cb0f705ef16eadf88dcbac4fcff6b04d
lovesh harchandani <lovesh.bond@gmail.com>
2018-09-11 13:52:54 +0530

    add bin2hex and hex2bin
    Signed-off-by: lovesh harchandani <lovesh.bond@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,8 @@
 //! Libsodium utility functions
 use ffi;
 
+use std::ptr::null;
+
 /// `memzero()` tries to effectively zero out the data in `x` even if
 /// optimizations are being applied to the code.
 pub fn memzero(x: &mut [u8]) {
@@ -57,11 +59,46 @@ pub fn increment_le(x: &mut [u8]) {
 pub fn add_le(x: &mut [u8], y: &[u8]) -> Result<(), ()> {
     if x.len() == y.len() {
         unsafe {
-            ffi::sodium_add(x.as_mut_ptr(), y.as_ptr(), x.len());
+                ffi::sodium_add(x.as_mut_ptr(), y.as_ptr(), x.len());
         }
         Ok(())
     } else  {
         Err(())
+    }
+}
+
+/// `bin2hex()` takes raw bytes as `bin`, converts it to hexadecimal and puts the result in `hex`.
+/// `hex` contains a nul byte (\0) terminator
+/// `bin2hex()` will return Err<()> if length of `hex` is not sufficient to hold the hex representation of `bin`.
+/// Refer docs at https://download.libsodium.org/doc/helpers#hexadecimal-encoding-decoding
+pub fn bin2hex(hex: &mut [u8], bin: &[u8]) -> Result<(), ()> {
+    if hex.len() <= 2*bin.len() {
+        return Err(());
+    }
+    unsafe {
+        ffi::sodium_bin2hex(hex.as_mut_ptr() as *mut _, 2*bin.len()+1, bin.as_ptr(), bin.len());
+    }
+    Ok(())
+}
+
+/// `hex2bin()` takes bytes of a hexadecimal string in `hex`, converts it to raw bytes and puts the result in `bin`
+/// `ignore` is an optional parameter, taking a byte representation of a string to skip
+/// `bin_len` is the length of raw bytes that `hex` was converted to.
+/// `hex_end` is an optional parameter, if passed a pointer, it will be set to the address of the
+/// first byte after the last valid parsed character.
+/// Refer docs at https://download.libsodium.org/doc/helpers#hexadecimal-encoding-decoding
+pub fn hex2bin(bin: &mut [u8], hex: &[u8], ignore: Option<&[u8]>, bin_len: &mut usize, hex_end: Option<&mut [u8]>) {
+    let ignore = match ignore {
+        Some(p) => p.as_ptr(),
+        None => null()
+    };
+    let hex_end = match hex_end {
+        Some(p) => p.as_mut_ptr(),
+        None => null()
+    };
+    unsafe {
+        ffi::sodium_hex2bin(bin.as_mut_ptr() as *mut _, bin.len(), hex.as_ptr() as *const _,
+                            hex.len(), ignore as *const _, bin_len, hex_end  as *mut _);
     }
 }
 
@@ -206,6 +243,63 @@ mod test {
             assert_eq!(x, vec!(1u8; i));
             assert!(add_le(&mut x, &z).is_err());
             assert_eq!(x, vec!(1u8; i));
+        }
+    }
+
+    #[test]
+    fn test_bin2hex() {
+        use std::str;
+
+        for (i, j) in vec![(0, "00\u{0}"), (1, "01\u{0}"), (10, "0a\u{0}"), (15, "0f\u{0}")] {
+            let bytes: [u8; 1] = [i];
+            let mut hex: [u8; 3] = [0; 3];
+            bin2hex(&mut hex, &bytes).unwrap();
+            assert_eq!(j, str::from_utf8(&hex).unwrap());
+        }
+    }
+
+    #[test]
+    fn test_hex2bin() {
+        for (i, j) in vec![(0, "00\u{0}"), (1, "01\u{0}"), (10, "0a\u{0}"), (15, "0f\u{0}")] {
+            let hex = j.as_bytes();
+            let mut bytes: [u8; 1] = [0];
+            let mut byte_size = 0;
+            hex2bin(&mut bytes, &hex, None, &mut byte_size, None);
+            assert_eq!(byte_size, 1);
+            assert_eq!(vec![i], bytes.to_vec());
+        }
+    }
+
+    #[test]
+    fn test_hex2bin_ignore() {
+        let mut bytes: [u8; 2] = [0, 0];
+        let mut byte_size = 0;
+        let ignore = ": ".as_bytes();
+        let expected_bytes = vec![105, 252];
+        for hex in vec!["69:FC".as_bytes(), "69 FC".as_bytes(), "69 : FC".as_bytes(), "69FC".as_bytes()] {
+            hex2bin(&mut bytes, &hex, Some(ignore), &mut byte_size, None);
+            assert_eq!(byte_size, 2);
+            assert_eq!(expected_bytes, bytes.to_vec());
+        }
+    }
+
+    #[test]
+    fn test_bin2hex_and_hex2bin() {
+        use randombytes::randombytes;
+        use super::super::init;
+
+        init().unwrap();
+
+        for i in 1..257 {
+            let bytes = randombytes(i);
+            let mut hex: Vec<u8> = vec![0; 2*i+1];
+            bin2hex(&mut hex, &bytes).unwrap();
+            assert_eq!(hex.len(), 2*i+1);
+            let mut new_bytes: Vec<u8> = vec![0; i];
+            let mut byte_size = 0;
+            hex2bin(&mut new_bytes, &hex, None, &mut byte_size, None);
+            assert_eq!(byte_size, i);
+            assert_eq!(&bytes, &new_bytes);
         }
     }
 }
