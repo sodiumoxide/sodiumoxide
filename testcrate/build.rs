39df6b85e4af02755005f08cc8b823a27b31d950
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2019-06-12 12:13:11 -0400

    Added DEP_SODIUM_INCLUDE & DEP_SODIUM_LIB cargo env var
    Add README to libsodium-sys
    Added testcrate for libsodium
    Fix CI to test all crates
    Add windows support
    Added RUST_BACKTRACE to appveyor
    Use sodium.h from the source_dir

---
+++ testcrate/build.rs
@@ -0,0 +1,42 @@
+use std::{env, fs};
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    let target = env::var("TARGET").unwrap();
+
+    // Skip the test when `SODIUM_LIB_DIR` is set since there is no
+    // build metadata.
+    if let Ok(_) = env::var("SODIUM_LIB_DIR") {
+        return;
+    }
+
+    let include = env::var("DEP_SODIUM_INCLUDE").unwrap();
+
+    let header = fs::read_dir(include)
+        .unwrap()
+        .filter_map(Result::ok)
+        .find(|entry| entry.file_name() == "sodium.h");
+    assert!(
+        header.is_some(),
+        "sodium.h not found in DEP_SODIUM_INCLUDE dir"
+    );
+
+    let lib = env::var("DEP_SODIUM_LIB").unwrap();
+
+    let file_name = if target.contains("msvc") {
+        "libsodium.lib"
+    } else {
+        "libsodium.a"
+    };
+
+    let compiled_lib = fs::read_dir(lib)
+        .unwrap()
+        .filter_map(Result::ok)
+        .find(|e| e.file_name() == file_name);
+
+    assert!(
+        compiled_lib.is_some(),
+        "compiled lib not found in DEP_SODIUM_LIB dir"
+    );
+}
