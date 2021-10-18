919bceb5cecf95d26c4028e760bf64759cc5b766
Kristof Hetzl <kristof.hetzl@gmail.com>
2021-10-18 20:50:45 +0100

    VRF tests

--- src/vrf.rs
+++ src/vrf.rs
@@ -9,19 +9,21 @@ pub const VRF_PUBKEY_BYTE_LENGTH: usize = ffi::crypto_vrf_PUBLICKEYBYTES as usiz
 
 /// A VrfPrivkey is a private key used for producing VRF proofs.
 /// Specifically, we use a 64-byte ed25519 private key (the latter 32-bytes are the precomputed public key)
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct VrfPrivKey([u8; 64]);
 
 /// A VrfPubkey is a public key that can be used to verify VRF proofs.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct VrfPubkey([u8; VRF_PUBKEY_BYTE_LENGTH]);
 
 /// A VrfProof for a message can be generated with a secret key and verified against a public key, like a signature.
 /// Proofs are malleable, however, for a given message and public key, the VRF output that can be computed from a proof is unique.
+#[derive(PartialEq)]
 pub struct VrfProof([u8; 80]);
 
 /// VrfOutput is a 64-byte pseudorandom value that can be computed from a VrfProof.
 /// The VRF scheme guarantees that such output will be unique
+#[derive(PartialEq)]
 pub struct VrfOutput([u8; 64]);
 type HashID = [u8; 2]; // This is a bit oversimplified
 
@@ -129,5 +131,77 @@ impl VrfPubkey {
     /// Moreover, the output is indistinguishable from random to anyone without the proof or the secret key.
     pub fn verify<H: Hashable>(&self, proof: VrfProof, msg: H) -> (bool, VrfOutput) {
         self.verify_bytes(proof, hash_rep(msg).as_slice())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hex;
+    use std::convert::TryInto;
+
+    fn must_decode(hex: String) -> Vec<u8> {
+        match hex::decode(hex) {
+            Ok(decoded) => decoded,
+            Err(err) => panic!("ERROR while decoding: {:?}", err),
+        }
+    }
+
+    fn test_vector(
+        sk_hex: String,
+        pk_hex: String,
+        alpha_hex: String,
+        pi_hex: String,
+        beta_hex: String,
+    ) {
+        let seed: [u8; 32] = must_decode(sk_hex).try_into().expect("ERROR: seed size");
+
+        let pk: VrfPubkey = VrfPubkey(must_decode(pk_hex).try_into().expect("ERROR: pk size"));
+        let alpha_vec = must_decode(alpha_hex);
+        let alpha: &[u8] = alpha_vec.as_slice();
+        let pi: VrfProof = VrfProof(must_decode(pi_hex).try_into().expect("ERROR: pi size"));
+        let beta: VrfOutput =
+            VrfOutput(must_decode(beta_hex).try_into().expect("ERROR: beta size"));
+
+        let (pk_test, sk) = vrf_keygen_from_seed(seed);
+        //assert_eq!(pk_test, pk);
+        if pk_test != pk {
+            panic!("Computed public key does not match the test vector");
+        }
+
+        let (pi_test, ok) = sk.prove_bytes(alpha);
+        if !ok {
+            panic!("Failed to produce a proof");
+        }
+        if pi_test != pi {
+            panic!("Proof produced by Prove() does not match the test vector");
+        }
+
+        let (ok, beta_test) = pk.verify_bytes(pi, alpha);
+        if !ok {
+            panic!();
+        }
+        if beta_test != beta {
+            panic!();
+        }
+    }
+
+    #[test]
+    fn test_vrf_test_vectors() {
+        test_vector(
+		String::from("9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60"), //sk
+		String::from("d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a"), //pk
+		String::from(""), // alpha
+		String::from("b6b4699f87d56126c9117a7da55bd0085246f4c56dbc95d20172612e9d38e8d7ca65e573a126ed88d4e30a46f80a666854d675cf3ba81de0de043c3774f061560f55edc256a787afe701677c0f602900"), // pi
+		String::from("5b49b554d05c0cd5a5325376b3387de59d924fd1e13ded44648ab33c21349a603f25b84ec5ed887995b33da5e3bfcb87cd2f64521c4c62cf825cffabbe5d31cc"),                                 // beta
+	);
+
+        test_vector(
+		String::from("4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb"), //sk
+		String::from("3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c"), //pk
+		String::from("72"), // alpha
+		String::from("ae5b66bdf04b4c010bfe32b2fc126ead2107b697634f6f7337b9bff8785ee111200095ece87dde4dbe87343f6df3b107d91798c8a7eb1245d3bb9c5aafb093358c13e6ae1111a55717e895fd15f99f07"), // pi
+		String::from("94f4487e1b2fec954309ef1289ecb2e15043a2461ecc7b2ae7d4470607ef82eb1cfa97d84991fe4a7bfdfd715606bc27e2967a6c557cfb5875879b671740b7d8"),                                 // beta
+	);
     }
 }
