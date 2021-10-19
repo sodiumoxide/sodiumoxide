299ba4c89f17cad17c2eb99bdd79da405ba0f89b
Kristof Hetzl <kristof.hetzl@gmail.com>
2021-10-19 18:26:53 +0100

    Tests

--- src/vrf.rs
+++ src/vrf.rs
@@ -18,12 +18,12 @@ pub struct VrfPubkey([u8; VRF_PUBKEY_BYTE_LENGTH]);
 
 /// A VrfProof for a message can be generated with a secret key and verified against a public key, like a signature.
 /// Proofs are malleable, however, for a given message and public key, the VRF output that can be computed from a proof is unique.
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub struct VrfProof([u8; 80]);
 
 /// VrfOutput is a 64-byte pseudorandom value that can be computed from a VrfProof.
 /// The VRF scheme guarantees that such output will be unique
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub struct VrfOutput([u8; 64]);
 type HashID = [u8; 2]; // This is a bit oversimplified
 
@@ -33,7 +33,6 @@ pub trait Hashable {
     fn to_be_hashed(&self) -> (HashID, &'static [u8]);
 }
 
-// FIXME: Hashable trait... or equivalent
 fn hash_rep<H: Hashable>(h: H) -> Vec<u8> {
     let (hashid, data) = h.to_be_hashed();
     let mut r = vec![];
@@ -164,26 +163,15 @@ mod tests {
             VrfOutput(must_decode(beta_hex).try_into().expect("ERROR: beta size"));
 
         let (pk_test, sk) = vrf_keygen_from_seed(seed);
-        //assert_eq!(pk_test, pk);
-        if pk_test != pk {
-            panic!("Computed public key does not match the test vector");
-        }
+        assert_eq!(pk_test, pk);
 
         let (pi_test, ok) = sk.prove_bytes(alpha);
-        if !ok {
-            panic!("Failed to produce a proof");
-        }
-        if pi_test != pi {
-            panic!("Proof produced by Prove() does not match the test vector");
-        }
+        assert!(ok);
+        assert_eq!(pi_test, pi);
 
         let (ok, beta_test) = pk.verify_bytes(pi, alpha);
-        if !ok {
-            panic!();
-        }
-        if beta_test != beta {
-            panic!();
-        }
+        assert!(ok);
+        assert_eq!(beta_test, beta);
     }
 
     #[test]
@@ -191,19 +179,19 @@ mod tests {
         ::init().unwrap();
 
         test_vector(
-		String::from("9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60"), //sk
-		String::from("d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a"), //pk
-		String::from(""), // alpha
-		String::from("b6b4699f87d56126c9117a7da55bd0085246f4c56dbc95d20172612e9d38e8d7ca65e573a126ed88d4e30a46f80a666854d675cf3ba81de0de043c3774f061560f55edc256a787afe701677c0f602900"), // pi
-		String::from("5b49b554d05c0cd5a5325376b3387de59d924fd1e13ded44648ab33c21349a603f25b84ec5ed887995b33da5e3bfcb87cd2f64521c4c62cf825cffabbe5d31cc"),                                 // beta
-	);
+            String::from("9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60"), //sk
+            String::from("d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a"), //pk
+            String::from(""), // alpha
+            String::from("b6b4699f87d56126c9117a7da55bd0085246f4c56dbc95d20172612e9d38e8d7ca65e573a126ed88d4e30a46f80a666854d675cf3ba81de0de043c3774f061560f55edc256a787afe701677c0f602900"), // pi
+            String::from("5b49b554d05c0cd5a5325376b3387de59d924fd1e13ded44648ab33c21349a603f25b84ec5ed887995b33da5e3bfcb87cd2f64521c4c62cf825cffabbe5d31cc"),                                 // beta
+        );
 
         test_vector(
-		String::from("4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb"), //sk
-		String::from("3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c"), //pk
-		String::from("72"), // alpha
-		String::from("ae5b66bdf04b4c010bfe32b2fc126ead2107b697634f6f7337b9bff8785ee111200095ece87dde4dbe87343f6df3b107d91798c8a7eb1245d3bb9c5aafb093358c13e6ae1111a55717e895fd15f99f07"), // pi
-		String::from("94f4487e1b2fec954309ef1289ecb2e15043a2461ecc7b2ae7d4470607ef82eb1cfa97d84991fe4a7bfdfd715606bc27e2967a6c557cfb5875879b671740b7d8"),                                 // beta
-	);
+            String::from("4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb"), //sk
+            String::from("3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c"), //pk
+            String::from("72"), // alpha
+            String::from("ae5b66bdf04b4c010bfe32b2fc126ead2107b697634f6f7337b9bff8785ee111200095ece87dde4dbe87343f6df3b107d91798c8a7eb1245d3bb9c5aafb093358c13e6ae1111a55717e895fd15f99f07"), // pi
+            String::from("94f4487e1b2fec954309ef1289ecb2e15043a2461ecc7b2ae7d4470607ef82eb1cfa97d84991fe4a7bfdfd715606bc27e2967a6c557cfb5875879b671740b7d8"),                                 // beta
+        );
     }
 }
